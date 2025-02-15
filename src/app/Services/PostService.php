da0020cbedbb6a6810bc87d8f0a990aebda7e80e
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-15 12:25:01 +0300

    added services

---
+++ src/app/Services/PostService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\PostDTO;
+use App\Models\Post;
+use App\Repository\Post\PostRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class PostService {
+    private PostRepositoryInterface $postRepository;
+
+    public function __construct(PostRepositoryInterface $postRepository) {
+        $this->postRepository = $postRepository;
+    }
+
+    public function getPaginatedAndFilteredPosts(?bool $isPublished, ?int $categoryId): LengthAwarePaginator {
+        return $this->postRepository->getPaginatedAndFilteredPosts($isPublished, $categoryId);
+    }
+
+    public function store(PostDTO $postDTO) {
+        return $this->postRepository->store($postDTO);
+    }
+
+    public function update(PostDTO $postDTO, Post $post) {
+        return $this->postRepository->update($postDTO, $post);
+    }
+
+    public function destroy(Post $post): void {
+        $this->postRepository->destroy($post);
+    }
+
+}
