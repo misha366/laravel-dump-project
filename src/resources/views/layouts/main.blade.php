55d4559f70c43930b3579ef15894ece4ad4b48cf
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-17 11:25:04 +0300

    added main layout

---
+++ src/resources/views/layouts/main.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    @vite(['resources/css/app.css'])
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+</head>
+<body class="pb-3">
+
+<div style="display: none" class="container page">
+    @yield('content')
+</div>
+
+
+@vite(['resources/js/app.js'])
+</body>
+</html>
--- src/resources/views/post/index.blade.php
+++ src/resources/views/post/index.blade.php
@@ -1,20 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/css/app.scss', 'resources/js/app.js'])
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <h3 class="col-3">Hello</h3>
-            <h3 class="col-3">Hello</h3>
-            <h3 class="col-3">Hello</h3>
-            <h3 class="col-3">Hello</h3>
-        </div>
+@extends('layouts.main')
+
+@section('title', 'Posts - Posts site')
+
+@section('content')
+
+
+<div class="container">
+    <div class="row">
+        <h3 class="col-3">Hello</h3>
+        <h3 class="col-3">Hello</h3>
+        <h3 class="col-3">Hello</h3>
+        <h3 class="col-3">Hello</h3>
     </div>
-</body>
-</html>
+</div>
+
+@endsection
 
