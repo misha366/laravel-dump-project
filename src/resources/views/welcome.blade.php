439942b2b66b11aa07f542271aa450e946277d05
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-18 18:16:49 +0300

    added elements to the main page

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -4,12 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Posts Site</title>
-
+    @vite(['resources/css/mainpage.css'])
 </head>
 <body>
-    
+
+    <div class="mainpage">
+        <h1 class="mainpage__title">Laravel Dump Project</h1>
+        <div class="mainpage__description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi, commodi!</div>
+        <a href="{{ route("posts.index") }}" class="btn btn-primary">Explore <i class="bi bi-rocket-takeoff"></i></a>
+    </div>
+
     <div id="tsparticles"></div>
-    
+
     @vite(['resources/js/homepage-particles.js'])
 </body>
 </html>
