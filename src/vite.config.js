3280b1c73e63c1d3715902524b340f4bcc424e38
misha366 <misha.moskalenko.jv@gmail.com>
2025-02-19 18:06:39 +0300

    added resources to vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/common.styles.css',
+                'resources/css/import.bootstrap.css',
+                'resources/css/mainpage.css',
+                'resources/css/setfont.css',
+                'resources/js/app.js',
+                'resources/js/homepage-particles.js',
+                'resources/js/preloader.js',
+                'resources/js/two-factor-challenge-toggler.js',
+            ],
             refresh: true,
         }),
     ],
